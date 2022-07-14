773e25fac5c14b512e9d806baebfd33e0edc49f9
sk8higher <sk8higher@iballwasrawt.ru>
2022-07-14 18:17:30 +0300

    added logics for opening/closing the modal

--- script.js
+++ script.js
@@ -2,3 +2,20 @@
 
 const btnAddBook = document.querySelector('#add-book-button');
 const booksGrid = document.querySelector('#books-grid');
+const modalWindow = document.querySelector('.add-book-modal');
+const modalOverlay = document.querySelector('.modal-overlay');
+const btnModalClose = document.querySelector('.add-book-modal__close-button');
+
+function showModal() {
+  modalWindow.classList.toggle('hidden');
+  modalOverlay.classList.toggle('hidden');
+}
+
+function closeModal() {
+  modalWindow.classList.toggle('hidden');
+  modalOverlay.classList.toggle('hidden');
+}
+
+btnAddBook.addEventListener('click', showModal);
+modalOverlay.addEventListener('click', closeModal);
+btnModalClose.addEventListener('click', closeModal);
